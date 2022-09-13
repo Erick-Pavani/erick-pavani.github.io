34dbf301e8d84f4279de0230f9e47183bd3e9512
Erick-Pavani <erickspavanii@gmail.com>
2022-09-13 10:46:59 -0300

    Now work in all touch devices no matter the size

--- js/projetos.js
+++ js/projetos.js
@@ -1,5 +1,4 @@
-if (window.innerWidth <= 500 ) {
-    
+if (isTouchDevice()) {
     const projetos = document.querySelectorAll(".projects_box");
     
     projetos.forEach(projeto => {
@@ -18,4 +17,33 @@ if (window.innerWidth <= 500 ) {
             })
         })
     });
+}
+
+window.addEventListener("resize", function() {
+    if (isTouchDevice()) {
+        const projetos = document.querySelectorAll(".projects_box");
+        
+        projetos.forEach(projeto => {
+            
+            projeto.addEventListener("click", function() {
+                projeto.querySelectorAll("a").forEach(link => {
+                    setTimeout(function () {
+                        link.style.pointerEvents = "auto";
+                    }, 500)
+                })
+            })
+            
+            projeto.addEventListener("mouseleave", function() {
+                projeto.querySelectorAll("a").forEach(link => {
+                    link.style.pointerEvents = "none";
+                })
+            })
+        });
+    }
+})
+
+function isTouchDevice() {
+    return (('ontouchstart' in window) ||  
+    (navigator.maxTouchPoints > 0) ||  
+    (navigator.msMaxTouchPoints > 0));  
 }
